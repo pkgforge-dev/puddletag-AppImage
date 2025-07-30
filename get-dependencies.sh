80e2ededcf03de08e6fce3cf322d7aa571f2d2d3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-07-30 17:17:28 -0400

    Create get-dependencies.sh

---
+++ get-dependencies.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+
+case "$ARCH" in
+	'x86_64')  PKG_TYPE='x86_64.pkg.tar.zst';;
+	'aarch64') PKG_TYPE='aarch64.pkg.tar.xz';;
+	''|*) echo "Unknown arch: $ARCH"; exit 1;;
+esac
+
+LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
+MESA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/mesa-mini-$PKG_TYPE"
+LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
+OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
+
+echo "Installing build dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	base-devel          \
+	curl                \
+	desktop-file-utils  \
+	fontconfig          \
+	freetype2           \
+	git                 \
+	libxcb              \
+	libxcursor          \
+	libxi               \
+	libxkbcommon        \
+	libxkbcommon-x11    \
+	libxrandr           \
+	libxtst             \
+	mesa                \
+	pipewire-audio      \
+	pulseaudio          \
+	pulseaudio-alsa     \
+	wget                \
+	xorg-server-xvfb    \
+	zsync
+
+
+echo "Installing debloated pckages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$LLVM_URL"   -O  ./llvm-libs.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$MESA_URL"   -O  ./mesa.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$LIBXML_URL" -O  ./libxml2.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$OPUS_URL"   -O  ./opus.pkg.tar.zst
+
+pacman -U --noconfirm ./*.pkg.tar.zst
+rm -f ./*.pkg.tar.zst
+
+echo "All done!"
+echo "---------------------------------------------------------------"
