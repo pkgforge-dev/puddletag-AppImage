0b29ea540a99fe331b20268c08df41531148b4a8
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-07-30 17:09:29 -0400

    big refactor, use sharun to deploy appimage

--- puddletag-appimage.sh
+++ puddletag-appimage.sh
@@ -1,100 +1,96 @@
-#!/usr/bin/env bash
-# archlinux deps: python musl base-devel patchelf gtk3 libglvnd qt5-base qt5-wayland qt5-svg
+#!/bin/sh
 
-set -eu
+set -ex
 
-PACKAGE="puddletag"
-export APPIMAGE_EXTRACT_AND_RUN=1
-export ARCH="$(uname -m)"
-UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
+ARCH="$(uname -m)"
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
+SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
+UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
+ICON="https://raw.githubusercontent.com/puddletag/puddletag/8a1d2badc76340f5cca84e938919707cc891c4ce/puddlestuff/data/appicon.svg"
 
-# MAKE APPDIR AND INSTALL PUDDLETAG
-mkdir -p ./"$PACKAGE"/build-env
-cd "$PACKAGE"
-python -m venv build-env
-. build-env/bin/activate
-python -m pip install --no-cache-dir --upgrade --force --ignore-installed pip
-python -m pip install --no-cache-dir --upgrade wheel pyinstaller
-python -m pip install --no-cache-dir --upgrade puddletag
+# Prepare AppDir
+mkdir -p ./AppDir && (
+	cd ./AppDir
+	# ADD LIBRARIES
+	wget --retry-connrefused --tries=30 "$SHARUN" -O ./sharun-aio
+	chmod +x ./sharun-aio
+	xvfb-run -a -- \
+		./sharun-aio l         \
+		--strip                \
+		--with-hooks           \
+		--python-ver 3.12      \
+		--python-pkg puddletag \
+		--dst-dir ./ sharun -- puddletag
+	rm -f ./sharun-aio
 
-VERSION="$(python -m pip show puddletag 2>/dev/null | awk '/Version:/ {print $2; exit}')"
-if [ -z "$VERSION" ]; then
-	echo "ERROR: Could not get version of puddletag"
-	exit 1
-fi
-echo "$VERSION" > ~/version
+	./sharun -g
 
-cp /usr/lib/ld-musl-x86_64.so.1 libc.musl-x86_64.so.1
+	wget "$ICON" -O ./puddletag.svg
+	cp -v ./puddletag.svg ./.DirIcon
 
-pyinstaller --name puddletag --target-arch x86_64 --strip --noupx \
-	--add-data build-env/lib/python*/site-packages/puddlestuff:puddlestuff \
-	--add-data /usr/lib/librt.so.1:. \
-	--add-data /usr/lib/libm.so.6:. \
-	--add-data /usr/lib/libxcb.so.1:. \
-	--add-data /usr/lib/libGLX.so.0:. \
-	--add-data /usr/lib/libGLdispatch.so.0:. \
-	--add-data /usr/lib/libGL.so.1:. \
-	--add-data libc.musl-x86_64.so.1:. \
-	"$(command -v puddletag)"
+	cat <<-'KEK' > ./AppRun
+	#!/bin/sh
+	CURRENTDIR="$(cd "${0%/*}" && echo "$PWD")"
+	export PATH="$CURRENTDIR/bin:$PATH"
+	[ -f "$APPIMAGE".stylesheet ] && APPIMAGE_QT_THEME="$APPIMAGE.stylesheet"
+	[ -f "$APPIMAGE_QT_THEME" ] && set -- "$@" "-stylesheet" "$APPIMAGE_QT_THEME"
+	exec "$CURRENTDIR"/bin/puddletag "$@"
+	EOF
+	chmod +x ./AppRun
 
-mkdir ./dist/puddletag/lib
-ldd dist/puddletag/puddletag | grep '> /' | sed 's|.*> /|/|g' \
-  | awk '{print $1}' | xargs -I {} cp -f {} dist/puddletag/lib/
-
-# add qt5ct
-cp "$(find /usr/lib -type f -name 'libqt5ct.so' -print -quit 2>/dev/null)" \
-  dist/puddletag/_internal/PyQt5/Qt5/plugins/platformthemes
-cp -r "$(find /usr/lib -type d -regex '.*plugins/styles' -print -quit 2>/dev/null)" \
-  dist/puddletag/_internal/PyQt5/Qt5/plugins
-
-shopt -s extglob
-patchelf --debug --set-rpath '$ORIGIN/lib' dist/puddletag/puddletag
-patchelf --debug --set-rpath '$ORIGIN' dist/puddletag/lib/!(ld-linux-x86-64.so.2)
-
-mv ./dist/puddletag ./AppDir
-
-echo '#!/usr/bin/env sh
-HERE="$(dirname "$(readlink -f "$0")")"
-[ -f "$APPIMAGE".stylesheet ] && APPIMAGE_QT_THEME="$APPIMAGE.stylesheet"
-[ -f "$APPIMAGE_QT_THEME" ] && set -- "$@" "-stylesheet" "$APPIMAGE_QT_THEME"
-exec "$HERE/lib/ld-linux-x86-64.so.2" "$HERE/puddletag" "$@"' > ./AppDir/AppRun
-chmod +x ./AppDir/AppRun
-
-cp -v ./build-env/share/applications/puddletag.desktop ./AppDir
-cp -v ./build-env/share/pixmaps/puddletag.png ./AppDir
-ln -s ./puddletag.png ./AppDir/.DirIcon
+	cat <<-'KEK' > ./puddletag.desktop
+	[Desktop Entry]
+	Version=1.0
+	Type=Application
+	Name=puddletag
+	TryExec=puddletag
+	MimeType=inode/directory;audio/x-ape;audio/x-wavpack;audio/x-musepack;audio/mpeg;audio/x-aiff;audio/x-dsf;audio/x-dff;audio/mp4;audio/x-m4a;video/mp4;video/x-m4v;audio/ogg;audio/flac;audio/x-opus+ogg;audio/x-ms-wma;video/x-ms-wmv;video/x-ms-asf;
+	Exec=puddletag %F
+	Icon=puddletag
+	GenericName=Audio Tag Editor
+	Categories=AudioVideo;Audio;Qt;
+	StartupWMClass=puddletag
+	Keywords=tagger;mp3tag;music;
+	KEK
+)
 
 # MAKE APPIMAGE WITH URUNTIME
-wget -q "$URUNTIME" -O ./uruntime
-chmod +x ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME"      -O  ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O  ./uruntime-lite
+chmod +x ./uruntime*
 
-#Add udpate info to runtime
+# Keep the mount point (speeds up launch time)
+sed -i 's|URUNTIME_MOUNT=[0-9]|URUNTIME_MOUNT=0|' ./uruntime-lite
+
+# Add udpate info to runtime
 echo "Adding update information \"$UPINFO\" to runtime..."
-./uruntime --appimage-addupdinfo "$UPINFO"
+./uruntime-lite --appimage-addupdinfo "$UPINFO"
 
 echo "Generating AppImage..."
-./uruntime --appimage-mkdwarfs -f \
-	--set-owner 0 --set-group 0 \
-	--no-history --no-create-timestamp \
+./uruntime \
+	--appimage-mkdwarfs -f               \
+	--set-owner 0 --set-group 0          \
+	--no-history --no-create-timestamp   \
 	--compression zstd:level=22 -S26 -B8 \
-	--header uruntime \
-	-i ./AppDir -o ./"$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
+	--header uruntime-lite               \
+	-i ./AppDir                          \
+	-o ./puddletag-"$VERSION"-anylinux-"$ARCH".AppImage
 
-wget -qO ./pelf "https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH" 
+# make appbundle
+UPINFO="$(echo "$UPINFO" | sed 's#.AppImage.zsync#*.AppBundle.zsync#g')"
+wget --retry-connrefused --tries=30 \
+	"https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH" -O ./pelf
 chmod +x ./pelf
-echo "Generating [dwfs]AppBundle...(Go runtime)"
-./pelf --add-appdir ./AppDir \
-	--appbundle-id="$PACKAGE-$VERSION" \
-	--compression "-C zstd:level=22 -S26 -B8" \
-	--output-to "$PACKAGE"-"$VERSION"-anylinux-"$ARCH".dwfs.AppBundle
+echo "Generating [dwfs]AppBundle..."
+./pelf \
+	--compression "-C zstd:level=22 -S26 -B8"      \
+	--appbundle-id="puddletag-$VERSION"            \
+	--appimage-compat --disable-use-random-workdir \
+	--add-updinfo "$UPINFO"                        \
+	--add-appdir ./AppDir                          \
+	--output-to ./puddletag-"$VERSION"-anylinux-"$ARCH".dwfs.AppBundle
 
-echo "Generating zsync file..."
-zsyncmake *.AppImage -u *.AppImage
-zsyncmake *.AppBundle -u *.AppBundle
-
-mv ./*.AppImage* ../
-mv ./*.AppBundle* ../
-cd ..
-rm -rf ./"$PACKAGE"
+zsyncmake ./*.AppImage -u ./*.AppImage
+zsyncmake ./*.AppBundle -u ./*.AppBundle
 echo "All Done!"
