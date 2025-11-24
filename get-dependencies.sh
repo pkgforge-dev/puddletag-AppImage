7faf08b5bf1768d410374e628fc38c122a299102
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-23 20:27:28 -0400

    use new template

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,37 +1,18 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+ARCH=$(uname -m)
 
-echo "Installing build dependencies..."
+echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	base-devel        \
-	curl              \
-	fontconfig        \
-	freetype2         \
-	git               \
-	libxcb            \
-	libxcursor        \
-	libxi             \
-	libxkbcommon      \
-	libxkbcommon-x11  \
-	libxrandr         \
-	libxtst           \
-	pulseaudio        \
-	pulseaudio-alsa   \
-	qt5-base          \
-	qt5ct             \
-	wget              \
-	xorg-server-xvfb  \
-	zsync
+# pacman -Syu --noconfirm PACKAGESHERE
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano
 
-echo "All done!"
-echo "---------------------------------------------------------------"
+# Comment this out if you need an AUR package
+make-aur-package puddletag-git
+
+# If the application needs to be manually built that has to be done down here
